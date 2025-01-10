943537b1b286dc172dbeb105ab8f523d7e0174c1
Anthony Petrosino <apetrosino789@gmail.com>
2025-01-10 01:14:34 -0500

    Update CI/CD

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import app
 def test_home():
     response=app.test_client().get("/")
 
-    # Successful request
+    # Tests if it recieves a successful request containing html
     assert response.status_code==200
     assert b"<!DOCTYPE html>" in response.data
